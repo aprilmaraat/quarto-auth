bd0ecb1addb367ce930d0ce9a76f2162f80eda6d
AJ_Maraat <amaraat@cyrious.com>
2020-01-28 21:13:39 +0800

    Initial login architecture.

--- Auth/Controllers/TokenController.cs
+++ Auth/Controllers/TokenController.cs
@@ -1,9 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Auth.Models.Entities;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Primitives;
+using Auth.Web.Models;
 
 namespace Auth.Web.Controllers
 {
     [Route("[controller]")]
     public class TokenController : Controller
     {
+        private readonly AuthContext _authContext;
+        //logging here
+
+        public TokenController(AuthContext authContext)
+        {
+            _authContext = authContext;
+        }
+
+        [HttpPost]
+        //[HttpOptions]
+        public async Task<IActionResult> Post([FromBody] PasswordTokenRequest loginRequest)
+        {
+            string userAgent = GetUserAgent();
+            return new UnauthorizedResult();
+        }
+
+        private string GetUserAgent() 
+        {
+            StringValues headers = Request.Headers["User-Agent"];
+            if (StringValues.IsNullOrEmpty(headers))
+                return null;
+            return headers[0];
+        }
     }
 }
