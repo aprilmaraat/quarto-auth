b45efd7a968c963199a3047bb96ae946e65a8085
April John Maraat <aprilmaraat@gmail.com>
2020-02-25 14:56:15 +0800

    Modified field case for response purpose.

--- Quarto.Auth.Api/Models/AuthResponse.cs
+++ Quarto.Auth.Api/Models/AuthResponse.cs
@@ -6,11 +6,8 @@ namespace Quarto.Auth.Api.Models
 {
     public class AuthResponse
     {
-        [JsonProperty("emailAddress")]
         public string EmailAddress { get; set; }
-        [JsonProperty("token")]
         public string Token { get; set; }
-        [JsonProperty("expiration")]
         public DateTime Expiration { get; set; }
     }
 }
