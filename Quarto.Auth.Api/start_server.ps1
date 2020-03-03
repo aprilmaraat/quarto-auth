fc61618e83284bfa06d4741c6961ccbddb1d21d0
April John Maraat <aprilmaraat@gmail.com>
2020-03-03 14:38:25 +0800

    Added start_server.ps1

---
+++ Quarto.Auth.Api/start_server.ps1
@@ -0,0 +1,8 @@
+﻿set-location "D:\Projects\quarto-auth\Quarto.Auth.Api"
+$launchProfile = 'LocalHTTPS';
+dotnet user-secrets list | ForEach-Object { [environment]::SetEnvironmentVariable("APPSETTING_"+$_.Substring(0,$_.IndexOf("=")),$_.Substring($_.IndexOf("=")+1));}
+$launchSettings = Get-Content .\Properties\launchSettings.json | ConvertFrom-Json;
+[environment]::SetEnvironmentVariable("ASPNETCORE_URLS", ($launchSettings | Select-Object -ExpandProperty "profiles" | Select-Object -ExpandProperty $launchProfile | Select-Object -ExpandProperty "applicationUrl"));
+[environment]::SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", ($launchSettings | Select-Object -ExpandProperty "profiles" | Select-Object -ExpandProperty $launchProfile | Select-Object -ExpandProperty "environmentVariables" | Select-Object -ExpandProperty "ASPNETCORE_ENVIRONMENT"));
+[environment]::SetEnvironmentVariable("ASPNETCORE_LAUNCHPROFILE", ($launchSettings | Select-Object -ExpandProperty "profiles" | Select-Object -ExpandProperty $launchProfile | Select-Object -ExpandProperty "environmentVariables" | Select-Object -ExpandProperty "ASPNETCORE_LAUNCHPROFILE"));
+dotnet run --Configuration Debug --launch-profile ([environment]::GetEnvironmentVariable("ASPNETCORE_LAUNCHPROFILE"))
