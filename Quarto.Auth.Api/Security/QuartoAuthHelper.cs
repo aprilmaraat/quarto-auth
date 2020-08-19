2019598a335354b553a38b03f5dcedf61ccbc305
April John Maraat <aprilmaraat@gmail.com>
2020-08-20 00:04:58 +0800

    Initial API security handler.

---
+++ Quarto.Auth.Api/Security/QuartoAuthHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace Quarto.Auth.Security
+{
+    public static class QuartoAuthHelper
+    {
+        private static void AddClaim(ClaimsIdentity identity, string type, string value)
+        {
+            if (value != null)
+            {
+                identity.AddClaim(new Claim(type, value));
+            }
+        }
+
+        //public static ClaimsPrincipal CreateIdentity()
+    }
+}
---
+++ Quarto.Auth.Api/Security/QuartoAuthOptions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authentication;
+
+namespace Quarto.Auth.Security
+{
+    public class QuartoAuthOptions : AuthenticationSchemeOptions
+    {
+        public const string DefaultScheme = "Quarto";
+        public const string SchemeDisplayName = "Quarto Scheme";
+    }
+}
